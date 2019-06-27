59e66bfa1b06bc3ca39a0f82f96139802ae5d0ad
Elber Castillo Mesa <elbercastillomesa@gmail.com>
2019-06-27 17:27:05 -0500

    in proccess to generate 'asesor.xlsx'

--- application/controllers/File.php
+++ application/controllers/File.php
@@ -27,19 +27,42 @@ class File extends CI_Controller {
  
     public function asesores()
     {
+        
+    	// Bring data from Model
+    	$data = $this->partners_model->get_asesores();
+
+    	// Create XLSX Spreadsheet
         $spreadsheet = new Spreadsheet();
         $sheet = $spreadsheet->getActiveSheet();
-        $sheet->setCellValue('A1', 'Hello World !');
-        
-        $writer = new Xlsx($spreadsheet);
- 
-        $filename = 'name-of-the-generated-file';
+        $writer = new Xlsx($spreadsheet); 
+        $filename = 'asesoresOndas_'.date('Y_m_d_H_i_s');
+
+        // Write data to Spreadsheet File
+
+        var_dump($data);
+        $sheet->setCellValue('A1', 'Nombres');
+        $sheet->setCellValue('B1', 'Apellidos');
+        $sheet->setCellValue('C1', 'Telefono');
+        $sheet->setCellValue('D1', 'Celular');
+        $sheet->setCellValue('E1', 'email');
+
+
+
+ $asesor['primer_nombre'].' '.$asesor['segundo_nombre'] ; ?></td>
+                <td><?php echo $asesor['primer_apellido'].' '.$asesor['segundo_apellido'] ; ?></td>
+                <td><?php echo $asesor['telefono'] ; ?></td>
+                <td><?php echo $asesor['celular'] ; ?></td>
+                <td><?php echo $asesor['asesor_email'] </td>
+
+
+
+        $sheet->setCellValue('A1', 'Hello World !');        
  
         header('Content-Type: application/vnd.ms-excel');
         header('Content-Disposition: attachment;filename="'. $filename .'.xlsx"'); 
         header('Cache-Control: max-age=0');
         
-        $writer->save('php://output'); // download file 
+        //$writer->save('php://output'); // download file 
  
     }
 
