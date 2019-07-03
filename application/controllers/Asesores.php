17b1b48523a99c3f157201dfb3e083e1b74be69b
Elber Castillo Mesa <elbercastillomesa@gmail.com>
2019-07-03 15:28:35 -0500

    remove unnecesary functions from asesores_helper due to new function to implement triying to make it more dinamyc

--- application/controllers/Asesores.php
+++ application/controllers/Asesores.php
@@ -26,7 +26,47 @@ class Asesores extends CI_Controller {
 		$this->load->view('templates/header', $data);
 		$this->load->view('templates/navbar', $data);		
 		$this->load->view('partners/asesores');
-		$this->load->view('templates/footer');		
+		$this->load->view('templates/footer');	
 	}
 
+	public function getData(){
+
+		// create the data object
+		$data = new stdClass();
+		
+		if ($this->form_validation->run() === false) {
+			
+			// validation not ok, send validation errors to the view
+			$data = showLinks($_SESSION, 'Asesores');
+			$data['lista'] = get_asesores();
+
+			$this->load->view('templates/header', $data);
+			$this->load->view('templates/navbar', $data);		
+			$this->load->view('partners/asesores');
+			$this->load->view('templates/footer');
+
+		} else {
+			
+			// set variables from the form
+			$array 			= $this->input->post('array');
+			$query_array 	= get_asesores($array);
+
+			$this->load->view('templates/header', $data);
+			$this->load->view('templates/navbar', $data);
+			
+			if ($query_array['result']) {
+				
+				$this->load->view('partners/asesores', $query_array['lista']);			
+				
+			} else {
+				
+				// Falla en la consulta
+				$data['error'] = 'Se present&oacute; un problema con los datos solicitados, intente nuevamente.';
+
+				$this->load->view('partners/asesores', $data['error']);	
+			}
+
+			$this->load->view('templates/footer');	
+		}
+	}
 }
--- application/helpers/asesores_helper.php
+++ application/helpers/asesores_helper.php
@@ -5,78 +5,9 @@
 function get_asesores(){
 
 	$CI =& get_instance();   
-	$CI->load->model('Asesores_model', 'asesores_model', TRUE);
-	
+	$CI->load->model('Asesores_model', 'asesores_model', TRUE);	
 	$data = $CI->asesores_model->get_asesores();
-
 	return $data;	
-}
-
-
-function books_page(){
-
-	// Datatables Variables
-	$draw = intval($this->input->get("draw"));
-	$start = intval($this->input->get("start"));
-	$length = intval($this->input->get("length"));
-	$books = $this->books_model->get_books();
-	$data = array();
-
-	foreach($books->result() as $r) {
-
-	$data[] = array(
-	$r->name,
-	$r->price,
-	$r->author,
-	$r->rating . "/10 Stars",
-	$r->publisher
-	);
-	}
-
-	$output = array(
-	"draw" => $draw,
-	"recordsTotal" => $books->num_rows(),
-	"recordsFiltered" => $books->num_rows(),
-	"data" => $data
-	);
-	echo json_encode($output);
-	exit();
-}
-
-function asesores_page(){
-
-  // Datatables Variables
-  $draw = '5';
-  $start = '1';
-  $length = '10';
-
-
-	$CI =& get_instance();   
-	$CI->load->model('Asesores_model', 'asesores_model', TRUE);
-
-	$asesores = $CI->asesores_model->get_asesores();
-
-
-  $data = array();
-
-  foreach($asesores as $r) {
-
-       $data[] = array(
-            'price' 	=> $r['id_asesor'],
-            'name' 		=> $r['primer_nombre'].' '.$r['primer_apellido'],
-            'author' 	=> $r['asesor_email'],
-            'rating' 	=> $r['celular']
-       );
-  }
-
-	$output = array(
-		"draw" => $draw,
-		"recordsTotal" => sizeof($asesores),
-		"recordsFiltered" => sizeof($asesores),
-		"data" => $data
-	);
-		
-	return $output;
 }
 
 ?>
