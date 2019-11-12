01e5dac5ddeef63e4cb4c99d78fa2d9081b6fb94
Elber Castillo Mesa <elbercastillomesa@gmail.com>
2019-11-12 17:32:47 -0500

    updated database

--- assets/tmp/tmp_ie.php
+++ assets/tmp/tmp_ie.php
@@ -22,7 +22,7 @@ while (($row = fgetcsv($fileHandle, 0, ";")) !== FALSE) {
     if(is_numeric($row[0])){
 
 
-    	$sql_IE = "SELECT `id_d_i`, `nit` FROM `o_datosinstitucion` WHERE nit LIKE '".$row[4]."'";
+    	$sql_IE = "SELECT `id_d_i`, `nit` FROM `o_datosinstitucion` WHERE nit LIKE '".$row[4]."%'";
     	$query_IE = $bd_con->insert($sql_IE);
 
     	var_dump($query_IE);
@@ -43,10 +43,10 @@ while (($row = fgetcsv($fileHandle, 0, ";")) !== FALSE) {
 			var_dump($row);	
 
 			break;
-
+/*
 			"INSERT INTO `o_institucion`(`id_institucion`, `nombre`, `codigo_institucion`, `fk_id_datosInst`, `fk_id_ver`, `fk_id_mpio`, `fk_id_sector`, `fk_id_tipo`, `fk_id_jor`) VALUES ()";
 
-			"array(9) { [0]=> string(5) "76036" [1]=> string(9) "ANDALUC�A" [2]=> string(12) "276036000037" [3]=> string(23) "IE AGRICOLA CAMPOALEGRE" [4]=> string(12) "276036000151" [5]=> string(18) "MARCO FIDEL SUAREZ" [6]=> string(5) "RURAL" [7]=> string(16) "CORREG ZABALETAS" [8]=> string(0) "" }";
+			"array(9) { [0]=> string(5) "76036" [1]=> string(9) "ANDALUC�A" [2]=> string(12) "276036000037" [3]=> string(23) "IE AGRICOLA CAMPOALEGRE" [4]=> string(12) "276036000151" [5]=> string(18) "MARCO FIDEL SUAREZ" [6]=> string(5) "RURAL" [7]=> string(16) "CORREG ZABALETAS" [8]=> string(0) "" }";*/
     }
     
 	
